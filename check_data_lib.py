a0b3512b6e24f39d8ace2fb06f2eac43adb265a7
Andrés Yagüe López <and.yague@gmail.com>
2021-03-28 20:36:03 +0200

    Made faster by avoiding innecesary calculation of gradients

--- check_data_lib.py
+++ check_data_lib.py
@@ -8,7 +8,7 @@ def apply_errors(arr, arr_err, nn):
     if nn > 0:
         new_arr = arr + np.random.random((nn, len(arr))) * 2 * arr_err - arr_err
     else:
-        new_arr = arr + np.random.random((2, len(arr))) * 0
+        new_arr = arr + np.random.random((1, len(arr))) * 0
 
     return new_arr
 
@@ -89,22 +89,19 @@ def get_distance(model, data):
 
     return dist
 
-def get_one_gradient(pow_mod, k_arr, x_k, log_x_k, data, k):
+def get_one_gradient(k_arr, coef, log_x_k, data, k):
     """
     Get gradients
     """
 
-    # Useful values
-    invLog = 2/np.log(10)
-
     # First find the appropriate index
     indx = np.searchsorted(k_arr, k)
 
-    # Now get diff
-    diff = log_x_k[indx] - data
+    # Now the whole gradient
+    diff = (log_x_k[indx] - data) * coef[indx]
 
-    # And the gradient
-    grad = np.mean((pow_mod - 1)/x_k[indx] * diff, axis = 1) * invLog
+    # And the mean
+    grad = np.mean(diff, axis = 1)
 
     return grad
 
@@ -119,35 +116,55 @@ def find_k(model, data, tol = 1e-3):
     # Make the array x_k 10 times finer than the tolerance
     step = tol * 0.1
     k_arr = np.arange(0, 1 + step, step)
+
+    # Get the x_k array
     x_k = np.array([(1 - k_arr) + k_arr * p for p in pow_mod]).T
+
+    # And now the really useful arrays
+    coef = (pow_mod - 1)/x_k * 2/np.log(10)
     log_x_k = np.log10(x_k)
 
     # Start with the extremes
-    k0 = 0
-    k1 = 1
-    km = 0.5
+    k0 = k_arr[0:1]
+    k1 = k_arr[-1:]
+    km = (k0 + k1) * 0.5
 
+    first = True
     while True:
 
         # Each of the gradients
-        grad0 = get_one_gradient(pow_mod, k_arr, x_k, log_x_k, data, k0)
-        gradm = get_one_gradient(pow_mod, k_arr, x_k, log_x_k, data, km)
-        grad1 = get_one_gradient(pow_mod, k_arr, x_k, log_x_k, data, k1)
+        gradm = get_one_gradient(k_arr, coef, log_x_k, data, km)
+        if first:
+            # If first time, we have to calculate everything
+            grad0 = get_one_gradient(k_arr, coef, log_x_k, data, k0)
+            grad1 = get_one_gradient(k_arr, coef, log_x_k, data, k1)
+
+            first = False
+        else:
+            # Otherwise we can use the previous calculation
+            grad0 = (sum0 * gradm + dif0 * grad0) * 0.5
+            grad1 = (sum1 * gradm + dif1 * grad1) * 0.5
 
         # Get signs
         sign0 = np.sign(grad0)
         signm = np.sign(gradm)
         sign1 = np.sign(grad1)
 
+        # Get differences
+        sum0 = np.abs(signm + sign0)
+        dif0 = np.abs(signm - sign0)
+        sum1 = np.abs(signm + sign1)
+        dif1 = np.abs(signm - sign1)
+
         # Vectorized change
-        k0 = np.abs(signm * (km + k0) + sign0 * (km - k0)) * 0.5
-        k1 = np.abs(signm * (km + k1) + sign1 * (km - k1)) * 0.5
+        k0 = (sum0 * km + dif0 * k0) * 0.5
+        k1 = (sum1 * km + dif1 * k1) * 0.5
 
         # Get the middle point
         new_km = (k0 + k1) * 0.5
 
         # Check if converged
-        dif = np.max(np.abs(new_km - km))
+        dif = np.abs(new_km[0] - km[0])
         if dif < tol:
             return new_km
 
