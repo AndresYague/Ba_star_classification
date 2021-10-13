a2fef33ebcab803de65fec5435eef47b9f36bb62
JacquelinedenHartogh <79260828+JacquelinedenHartogh@users.noreply.github.com>
2021-10-13 13:13:47 +0200

    Add files via upload
    made function that loads ba stars part of this file

--- data_processing_and_plotting/process_data_lib.py
+++ data_processing_and_plotting/process_data_lib.py
@@ -149,7 +149,7 @@ def get_data_values(data_file, names=None):
                         names.append(elem)
                     else:
                         names.append(elem + "/Fe")
-
+                        
         # Rest
         all_vals = {}
         for line in fread:
@@ -322,6 +322,7 @@ def apply_dilution(elems, kk, names, zero=0):
 
     # Just apply the formula to each element in name
     new_elements = {}
+
     for name in elems:
 
         # Skip metallicity
@@ -337,10 +338,11 @@ def apply_dilution(elems, kk, names, zero=0):
 
     # It may happen that all the abundances are very low, so skip those cases
     all_zero = True
+
     for name in names:
 
         # Skip metallicity
-        if name == "Fe/H":
+        if name == "Fe/H": 
             continue
 
         if abs(new_elements[name]) > zero:
@@ -396,6 +398,9 @@ def get_clean_lnlst_final(line):
     elif 'probability' in line:
         name = name_check(lnlst[1])
         return [name, lnlst[5], lnlst[7]]
+
+    elif 'Fail' in line:
+        return None
 
     # Group label and number of matches
     elif len(lnlst) == 2:
@@ -487,3 +492,47 @@ def name_check(name, dir_=None):
         pass
 
     return name
+
+def load_ba_stars(file_data):
+    # Now load Ba stars data
+    missing_values = dict()
+    all_data = []; all_names = []; all_errors = []
+    with open(file_data, "r") as fread:
+        header = fread.readline().split()[1:]
+        for line in fread:
+            lnlst = line.split()
+            all_names.append(lnlst[-1])
+
+            # Put values in an array
+            arr = []; arr_err = []; missing = []
+            for ii in range(len(header)):
+                # Skip name
+                if "Name" in header[ii]:
+                    continue
+
+                # Transform to float
+                try:
+                    val = float(lnlst[ii])
+                except ValueError:
+                    val = 1
+                    missing.append(header[ii])
+                except:
+                    raise
+
+                # Put errors and values in different arrays
+                if "_err" in header[ii]:
+                    arr_err.append(val)
+                else:
+                    arr.append(val)
+
+            # Convert to numpy arrays
+            arr = np.array(arr)
+            arr_err = np.array(arr_err)
+
+            # Save data
+            all_data.append(arr)
+            all_errors.append(arr_err)    
+            if len(missing) > 0:
+                missing_values[lnlst[-1]]=missing
+
+    return all_data, all_errors, all_names, missing_values
